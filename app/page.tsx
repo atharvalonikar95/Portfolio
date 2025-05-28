c7f6d142e8338ca099445459c047d77f9fa7a28d
atharva lonikar <atharvalonikar95@gmail.com>
2025-05-29 00:51:47 +0530

    29-5-25

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className='text-red-800 text-2xl outline-2 outline-black p-2 m-1 text-center'>
+      HomePage
+    </div>
+  )
+}
+
+export default HomePage
