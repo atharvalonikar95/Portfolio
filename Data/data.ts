14584a6bdb7224b06b29febb6be2337c096eac69
atharva lonikar <atharvalonikar95@gmail.com>
2025-09-17 00:45:06 +0530

    17-09-25

--- Data/data.ts
+++ Data/data.ts
@@ -37,38 +37,38 @@ export const servicesData = [
       "Seamless integration for smooth data sharing and functionality.",
     icon: "/images/s3.png",
   },
-  {
-    id: 4,
-    title: "User Interfaces",
-    description:
-      "Engaging, responsive front-end designs for better user experiences.",
-    icon: "/images/s4.png",
-  },
-  {
-    id: 5,
-    title: "Database Solutions",
-    description:
-      "Efficient and scalable database management for your data needs.",
-    icon: "/images/s5.png",
-  },
-  {
-    id: 6,
-    title: "Real-Time Features",
-    description: "Add live chat, notifications, and real-time data updates.",
-    icon: "/images/s6.png",
-  },
-  {
-    id: 7,
-    title: "Speed Optimization",
-    description: "Boost performance with faster load times and optimization.",
-    icon: "/images/s7.png",
-  },
-  {
-    id: 8,
-    title: "Website Management",
-    description: "Easy content management systems with flexible controls.",
-    icon: "/images/s8.png",
-  },
+  // {
+  //   id: 4,
+  //   title: "User Interfaces",
+  //   description:
+  //     "Engaging, responsive front-end designs for better user experiences.",
+  //   icon: "/images/s4.png",
+  // },
+  // {
+  //   id: 5,
+  //   title: "Database Solutions",
+  //   description:
+  //     "Efficient and scalable database management for your data needs.",
+  //   icon: "/images/s5.png",
+  // },
+  // {
+  //   id: 6,
+  //   title: "Real-Time Features",
+  //   description: "Add live chat, notifications, and real-time data updates.",
+  //   icon: "/images/s6.png",
+  // },
+  // {
+  //   id: 7,
+  //   title: "Speed Optimization",
+  //   description: "Boost performance with faster load times and optimization.",
+  //   icon: "/images/s7.png",
+  // },
+  // {
+  //   id: 8,
+  //   title: "Website Management",
+  //   description: "Easy content management systems with flexible controls.",
+  //   icon: "/images/s8.png",
+  // },
 ];
 
 export const projectData = [
